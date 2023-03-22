d1bcdc6954e27063c1aaa41f7dd81efe15a46c8a
bdnrJ <jbednarczyk001@gmail.com>
2023-03-22 09:23:18 +0100

    client: add register ui,add client side validation

--- react/src/views/Register.tsx
+++ react/src/views/Register.tsx
@@ -21,28 +21,40 @@ const Register = () => {
     const navigate = useNavigate();
 
     const schema: ZodType<User> = z.object({
-        firstname: z.string().min(1, 'required'),
-        lastname: z.string().min(1, 'required'),
+        firstname: z.string().min(1, 'Required'),
+        lastname: z.string().min(1, 'Required'),
         email: z.string().email(),
-        pwd: z.string().min(8, 'too short').max(30, 'too long').regex(
+        pwd: z.string().min(8, 'Too short').max(30, 'Too long').regex(
             /^(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
-            "at least one upper/lowercase letter"
+            "bad format"
         ),
         confirmPwd: z.string().min(1, 'required')
     }).refine((data) => data.pwd === data.confirmPwd, {
-        message: "Passwords dont match",
-        path: ["confirmPassword"],
+        message: "Not equals",
+        path: ["confirmPwd"],
     })
 
-    const { register, watch, formState: { errors } } = useForm<User>({resolver: zodResolver(schema)});
+    const { register, handleSubmit,watch, formState: { errors } } = useForm<User>({resolver: zodResolver(schema)});
 
     console.log(watch(['firstname','lastname','email','pwd','confirmPwd']));
 
+    const onSubmit = (data: User) => handleRegister(data);
 
-    const onSubmit = (e: any) =>{
-        e.preventDefault();
+    const handleRegister = async (data: User) =>{
+        try{
+        const response = await axiosClient.post('/api/register',
+        {firstname: data.firstname, lastname: data.lastname, email: data.email, pwd: data.pwd});
+        setSuccess("Registered succesfully");
+        setRegisterErr("");
+        setDisabled(true);
+        setTimeout(() => navigate('/'), 1500);
+        }catch(err: any){
+            console.log(err);
+            setRegisterErr(err.response.data.message);
+        }
+    }
 
-
+        
         // axiosClient.post('http://localhost:8000/api/register', payload)
         //     .then(({data}) => {
         //         console.log("then part of register");
@@ -53,7 +65,6 @@ const Register = () => {
         //         const res = err.response;
         //         console.log(res);
         //     })
-    }
 
     return (
         <div className="register">
@@ -67,50 +78,54 @@ const Register = () => {
                         Sign up
                     </div>
                 </div>
-                <form onSubmit={onSubmit}>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-inputwrapper">
                         <span>Firstname</span>
                         <label htmlFor="firstname">
-                            <input className={`_formInput`} type="text" {...register("firstname", {required: true})}/>
+                            <input className={`_formInput ${errors.firstname && "--error"}`} type="text" {...register("firstname", {required: true})}/>
                             {errors.firstname && <span className={`_inputError`}>{errors.firstname.message}</span>}
                         </label>
                     </div>
                     <div className="form-inputwrapper">
                         <span>Lastname</span>
                         <label htmlFor="lastname">
-                            <input className={`_formInput`} type="text" {...register("lastname", {required: true})}/>
+                            <input className={`_formInput ${errors.lastname && "--error"}`} type="text" {...register("lastname", {required: true})}/>
                             {errors.lastname && <span className={`_inputError`}>{errors.lastname.message}</span>}
                         </label>
                     </div>
                     <div className="form-inputwrapper">
                         <span>Email</span>
                         <label htmlFor="email">
-                            <input className={`_formInput`} type="text" {...register("email", {required: true})}/>
+                            <input className={`_formInput ${errors.email && "--error"}`} type="text" {...register("email", {required: true})}/>
                             {errors.email && <span className={`_inputError`}>{errors.email.message}</span>}
                         </label>
                     </div>
                     <div className="form-inputwrapper">
                         <span>Password</span>
                         <label htmlFor="pwd">
-                            <input className={`_formInput`} type="text" {...register("pwd", {required: true})}/>
+                            <input className={`_formInput ${errors.pwd && "--error"}`} type="password" {...register("pwd", {required: true})}/>
                             {errors.pwd && <span className={`_inputError`}>{errors.pwd.message}</span>}
                         </label>
+                        <span className="password-requirements" >At least 8 characters, one lower letter, one upper letter</span>
                     </div>
                     <div className="form-inputwrapper">
                         <span>Confirm Password</span>
                         <label htmlFor="confirmPwd">
-                            <input className={`_formInput`} type="text" {...register("confirmPwd", {required: true})}/>
+                            <input className={`_formInput ${errors.confirmPwd && "--error"}`} type="password" {...register("confirmPwd", {required: true})}/>
                             {errors.confirmPwd && <span className={`_inputError`}>{errors.confirmPwd.message}</span>}
                         </label>
                     </div>
 
                     <div className="register-form-button">
-                        <input type="submit" value="Sign in" />
+                        <input disabled={disabled} type="submit" value="Sign in" className='_confirmButton' />
                     </div>
                 </form>
                 <div className="register-form-redirect">
                     <span>Already have an account?</span>
-                    <span><Link className='_styledLink' to="/login">Log in</Link></span>
+                    <span>
+                        {disabled ? <span className='_link'>Log in</span>
+                                :<Link className='_link' to="/login">Log in</Link>}
+                    </span>
                 </div>
             </div>
         </div>
