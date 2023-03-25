363e34876f985236ba8f1768e1d716e00ef7e5d1
bdnrJ <jbednarczyk001@gmail.com>
2023-03-26 00:47:22 +0100

    client: ui home page

--- react/src/views/Home/Home.tsx
+++ react/src/views/Home/Home.tsx
@@ -1,12 +1,54 @@
 import React, {useState} from 'react'
 import SlideShow from './SlideShow'
+import {GoLocation} from 'react-icons/Go'
+import {GiTicket} from 'react-icons/gi'
+import { BsTelephoneFill } from 'react-icons/Bs'
 
 const Home = () => {
     return (
         <div className="home">
             <SlideShow />
             <div className="home-info">
-                home info
+                <div className="info-wrapper">
+
+                    <div className="info-block">
+                        <div className="info-block-icon yellow">
+                            <GoLocation />
+                        </div>
+                        <div className="info-block-title tyellow">
+                            Getting Here
+                        </div>
+                        <div className="info-block-desc">
+                            <span>Check how to find us</span>
+                        </div>
+                    </div>
+
+                    <div className="info-block">
+                        <div className="info-block-icon green">
+                            <BsTelephoneFill />
+                        </div>
+                        <div className="info-block-title tgreen">
+                            Contact
+                        </div>
+                        <div className="info-block-desc">
+                            <span>+123 123 123 321</span>
+                            <span>ZooProjekt@email.com</span>
+                        </div>
+                    </div>
+
+                    <div className="info-block">
+                        <div className="info-block-icon red">
+                            <GiTicket/>
+                        </div>
+                        <div className="info-block-title tred">
+                            Tickets
+                        </div>
+                        <div className="info-block-desc">
+                            <span>Informations about tickets</span>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
