9c0c232f01160a641568d8d3ebd8304d49c43cc7
bdnrJ <jbednarczyk001@gmail.com>
2023-05-10 12:13:02 +0200

    server: add user SoftDelete feature

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->boolean('role')->default(false);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
