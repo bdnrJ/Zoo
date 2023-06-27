89963a8e286f787771774e5a6cb5eaf4d49f7787
bdnrJ <jbednarczyk001@gmail.com>
2023-06-27 19:59:25 +0200

    script, readme updates

--- start.bat
+++ start.bat
@@ -1,7 +1,3 @@
-%systemDrive%\xampp\mysql\bin\mysql -uroot -e "CREATE DATABASE IF NOT EXISTS zoodb;"
-
-if %errorlevel% neq 0 msg %username% "Nie udalo sie utworzyc bazy danych." && exit /b %errorlevel%
-
 php -r "copy('.env.example', '.env');"
 
 call composer install
